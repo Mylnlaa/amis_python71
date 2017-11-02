c9799b1dff324a2990137303e1ead6ff9f85b7e8
Milena Lukina <32573815+Mylnlaa@users.noreply.github.com>
2017-11-02 16:37:04 +0200

    Delete task1.ру.py

--- km71/Lukina_Milena/task1.ру.py
+++
@@ -1,6 +0,0 @@
-number1 = float(input("Введите значение первого числа:"))
-number2 = float(input("Введите значение второго числа:"))
-number3 = float(input("Введите значение третьего числа:"))
-res = number1 + number2 + number3
-print("Ваш результат = " + str(res))
-print(input("Нажмите клавишу \"Enter\" для окончания работы программы"))
