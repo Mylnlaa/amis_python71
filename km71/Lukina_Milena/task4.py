c6f5741ac6a8895c141dd1bd9466f74c2581cc03
Milena Lukina <32573815+Mylnlaa@users.noreply.github.com>
2017-11-02 16:58:26 +0200

    Add files via upload

---
+++ km71/Lukina_Milena/task4.py
@@ -0,0 +1,8 @@
+apples = int(input("Введите кол-во яблок:"))
+students = int(input("Введите кол-во студентов:"))
+res1 = apples//students
+res2 = apples%students
+print("Количество яблок у каждого студента: " + str(res1), "\nКоличество яблок в корзине: " + str(res2))
+print(input("Нажмите клавишу \"Enter\" для окончания работы программы"))
+
+
