a1bb787f706dea08e96a5faec59c0d1112d7a683
Milena Lukina <32573815+Mylnlaa@users.noreply.github.com>
2017-11-02 16:43:47 +0200

    Delete task4.py

--- km71/Lukina_Milena/task4.py
+++
@@ -1,8 +0,0 @@
-apples = int(input("Введите кол-во яблок:"))
-students = int(input("Введите кол-во студентов:"))
-res1 = apples//students
-res2 = apples%students
-print("Количество яблок у каждого студента: " + str(res1), "\nКоличество яблок в корзине: " + str(res2))
-print(input("Нажмите клавишу \"Enter\" для окончания работы программы"))
-
-
