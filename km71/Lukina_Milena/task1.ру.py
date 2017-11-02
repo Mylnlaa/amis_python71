a4db318bad3edcabac19193d80b27878b1ff2ae6
Milena Lukina <32573815+Mylnlaa@users.noreply.github.com>
2017-11-02 16:32:17 +0200

    3
    Homework

---
+++ km71/Lukina_Milena/task1.ру.py
@@ -0,0 +1,6 @@
+number1 = float(input("Введите значение первого числа:"))
+number2 = float(input("Введите значение второго числа:"))
+number3 = float(input("Введите значение третьего числа:"))
+res = number1 + number2 + number3
+print("Ваш результат = " + str(res))
+print(input("Нажмите клавишу \"Enter\" для окончания работы программы"))
